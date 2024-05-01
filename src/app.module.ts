6ae3f9fc9442865458aac7520951bcddca56804c
tommypria <tommypriambodo@gmail.com>
2024-05-01 23:41:50 +0700

    logger module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { TypeOrmModule } from './infra/config/type-orm/type-orm.module';
 import { RepositoriesModule } from './infra/repositories/repositories.module';
 import { UseCasesProxyModule } from './infra/use-cases-proxy/use-cases-proxy.module';
 import { ControllersModule } from './infra/controllers/controllers.module';
+import { LoggerModule } from './infra/logger/logger.module';
 
 @Module({
   imports: [
     UseCasesProxyModule.register(),
     TypeOrmModule,
     ControllersModule,
+    LoggerModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/infra/logger/logger.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { LoggerInterface } from 'src/domain/logger/logger.interface';
+
+@Injectable()
+export class LoggerService extends Logger implements LoggerInterface {
+    debug(context: string, message: string): void {
+        super.debug(`[DEBUG] ${message}`, context);    
+    }
+
+    log(context: string, message: string): void {
+        super.log(`[INFO] ${message}`, context);
+    }
+
+    error(context: string, message: string, trace?: string): void{
+        super.error(`[ERROR] ${message}`, trace, context);
+    }
+
+    warn(context: string, message: string): void {
+        super.warn(`[WARN] ${message}`, context);
+    }
+
+    verbose(context: string, message: string): void {
+        super.verbose(`[VERBOSE] ${message}`, context);
+    }
+}
