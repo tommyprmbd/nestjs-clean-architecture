975713f8fda80228394797ec70c2a9610c6346c2
tommypria <tommypriambodo@gmail.com>
2024-06-24 13:10:30 +0700

    test: user delete test case

--- test/unit/usecase/users/user-delete-usecase.spec.ts
+++ test/unit/usecase/users/user-delete-usecase.spec.ts
@@ -1,21 +1,39 @@
+import { userInterfaceMock } from "./../../../mock/domain/model/user-interface.mock"
 import { UserRepositoryInterface } from "./../../../../src/domain/repository/user.repository.interface"
 import { UserDeleteUseCase } from "./../../../../src/usecase/users"
 import { userRepositoryInterfaceMock } from "./../../../mock/domain/repository/user-repository-interface.mock"
+import { DeleteResultDtoInterface } from "./../../../../src/domain/dtos"
+import { deleteResultDtoInterfaceMock } from "./../../../mock/domain/dtos/result/delete-result-dto-interface.mock"
 
 describe('UserDeleteUseCase', () => {
-    let userDeleteUseCase: UserDeleteUseCase
+  let userDeleteUseCase: UserDeleteUseCase
 
-    let userRepositoryInterface: UserRepositoryInterface = userRepositoryInterfaceMock
+  let userRepositoryInterface: UserRepositoryInterface = userRepositoryInterfaceMock
+  let deleteResultDtoInterface: DeleteResultDtoInterface = deleteResultDtoInterfaceMock
 
-    beforeEach(() => {
-        userDeleteUseCase = new UserDeleteUseCase(userRepositoryInterface)
+  beforeEach(() => {
+    userDeleteUseCase = new UserDeleteUseCase(userRepositoryInterface)
+  })
+
+  it('should be defined', () => {
+    expect(userDeleteUseCase)
+  })
+
+  describe('execute()', () => {
+    describe('repository.findById()', () => {
+      it('should return User model', async () => {
+        expect(await userRepositoryInterface.findById(1)).toBe(userInterfaceMock)
+      })
     })
 
-    it('should be defined', () => {
-        expect(userDeleteUseCase)
+    describe('repository.delete()', () => {
+      it('should return DeleteResultDtoInterface', async () => {
+        expect(await userRepositoryInterface.delete(1)).toBe(deleteResultDtoInterface)
+      })
     })
+  })
 
-    describe('execute()', () => {
-
-    })
+  it('should return DeleteResultDtoInterface', async () => {
+    expect(await userRepositoryInterface.delete(1)).toBe(deleteResultDtoInterface)
+  })
 })
